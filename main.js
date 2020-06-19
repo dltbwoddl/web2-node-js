6426017544ba9418a0bcc772391b3b3cf57d2a45
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-06-19 12:46:08 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,41 +1,22 @@
-//ajax를 활용해 query string변화시 해당 txt 파일을 불러와 페이지를 생성하는 방법.
-//abs.txt를 불러왔는데 해당 페이지의 html코드 전체가 나온다.?? 이유 알아보기.
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var http = require('http');//To use the HTTP server and client one must require('http').
+var fs = require('fs');//The fs module provides an API for interacting with the file system in a manner closely modeled around standard POSIX functions.
+var url = require('url');//The url module provides utilities for URL resolution and parsing.
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title =queryData.id
-    console.log(queryData.id);
-    if(_url == '/'){
-title = 'welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    var template =`
+    if (pathname=='/'){      
+    fs.readFile('data/'+`${queryData.id}`+'.txt','utf8',function(err,describesion){
+      var template =`
     <!doctype html>
 <html>
 <head>
-<script src="https://code.jquery.com/jquery-1.12.4.min.js">
-</script>  
   <title>WEB1 - ${title}</title>
   <meta charset="utf-8">
 </head>
 <body>
-<script>
-var id ='${title}';
-if(id==='HTML'){
-  fetch('ads.txt').then(function(response){
-    response.text().then(function(text){
-      document.querySelector('article').innerHTML = text;
-    })
-  })
-}
-</script>
   <h1><a href="/">WEB</a></h1>
   <ul>
     <li><a href="/?id=HTML">HTML</a></li>
@@ -43,16 +24,18 @@ if(id==='HTML'){
     <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
   </ul>
   <h2>${title}</h2>
-  <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-  <img src="coding.jpg" width="100%">
-  </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+  <p>
+  ${describesion}
   </p>
-  <article></article>
 </body>
 </html>
 `
-    console.log(__dirname + _url)
+    response.writeHead(200);
     response.end(template);
- 
+  });}
+  else{
+    response.writeHead(404);
+    response.end('not page found');
+  }
 });
 app.listen(3000);
